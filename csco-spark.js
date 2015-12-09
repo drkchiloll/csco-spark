73901a05d5ac982a031b3d56b8ea38093b0a0c17
drkchiloll <samwomack_@me.com>
2015-12-09 16:00:47 -0600

    trying to keep it dry..still not cohesive..but by choice

--- csco-spark.js
+++ csco-spark.js
@@ -1,7 +1,7 @@
 var request = require('request'),
     Promise = require('bluebird');
 
-function reqOptions(options) {
+function _reqOptions(options) {
   return {
     url: options.uri,
     headers: {
@@ -13,8 +13,8 @@ function reqOptions(options) {
   };
 }
 
-function makeReq(args) {
-  var options = reqOptions({
+function _makeReq(args) {
+  var options = _reqOptions({
     uri: args.uri, token: args.token
   });
   options.method = args.method;
@@ -34,51 +34,51 @@ module.exports = function(params) {
   var uri = params.uri,
       token = params.token;
 
-  var handler = {};
-
-  handler.createRoom = function(room) {
-    return makeReq({
+  var _handleReq = (params) => {
+    return _makeReq({
       uri: uri,
       token: token,
-      path: '/rooms',
+      path: params.path,
+      method: params.method,
+      body: params.body || ''
+    });
+  };
+
+  var handler = {};
+
+  handler.createRoom = function(roomProps) {
+    return _handleReq({
+      path: `/rooms`,
       method: 'POST',
-      body: room
+      body: roomProps
     });
   };
 
   handler.removeRoom = function(roomId) {
-    return makeReq({
-      uri: uri,
-      token: token,
+    return _handleReq({
       path: `/rooms/${roomId}`,
       method: 'DELETE'
     });
   };
 
-  handler.sendMessage = function(message) {
-    return makeReq({
-      uri: uri,
-      token: token,
-      path: '/messages',
+  handler.sendMessage = function(messageProps) {
+    return _handleReq({
+      path: `/messages`,
       method: 'POST',
-      body: message
+      body: messageProps
     });
   };
 
   handler.getPerson = function(options) {
     var userEmail = options.email;
-    return makeReq({
-      uri: uri,
-      token: token,
+    return _handleReq({
       path: `/people?email=${userEmail}`,
       method: 'GET'
     });
   };
 
   handler.addMemberToRoom = function(member) {
-    return makeReq({
-      uri: uri,
-      token: token,
+    return _handleReq({
       path: '/memberships',
       method: 'POST',
       body: member
@@ -86,9 +86,7 @@ module.exports = function(params) {
   };
 
   handler.addUserToRoom = function(args) {
-    return makeReq({
-      uri: uri,
-      token: token,
+    return _handleReq({
       path: `/rooms/${args.roomId}/participants`,
       method: 'POST',
       body: args.participants
@@ -96,36 +94,28 @@ module.exports = function(params) {
   };
 
   handler.removeUserFromRoom = function(id) {
-    return makeReq({
-      uri: uri,
-      token: token,
+    return _handleReq({
       path: `/memberships/${id}`,
       method: 'DELETE'
     })
   };
 
   handler.getMessage = function(messageId) {
-    return makeReq({
-      uri: uri,
-      token: token,
+    return _handleReq({
       path: `/messages/${messageId}`,
       method: 'GET'
     });
   };
 
   handler.getMessages = function(options) {
-    return makeReq({
-      uri: uri,
-      token: token,
+    return _handleReq({
       path: `/messages?roomId=${options.roomId}&max=1000`,
       method: 'GET'
     });
   };
 
   handler.addWebhook = function(data) {
-    return makeReq({
-      uri: uri,
-      token: token,
+    return _handleReq({
       path: '/webhooks',
       method: 'POST',
       body: data
@@ -133,9 +123,7 @@ module.exports = function(params) {
   };
 
   handler.removeWebHook = function(webhookId) {
-    return makeReq({
-      uri: uri,
-      token: token,
+    return _handleReq({
       path: `/webhooks/${webhookId}`,
       method: 'DELETE'
     });
