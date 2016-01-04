186628007dfc2f4b3b74335c9bea1c76ffa1b917
drkchiloll <samwomack_@me.com>
2016-01-04 08:53:15 -0600

    getFileUri streamline/rewrite

--- examples/dlFiles.js
+++ examples/dlFiles.js
@@ -5,20 +5,27 @@ var spark = require('../csco-spark')({
   token: config.token
 });
 
-spark.getMessages({roomId: config.roomId}).then(function(data) {
+// spark.getMessages({roomId: config.roomId}).then(function(data) {
+//   var messages = data;
+//
+//   var fileUris = messages
+//     .filter((message) => message.files)
+//     .map((message) => message.files)
+//     .reduce((arr, files) => arr.concat(files), []);
+//
+//   return Promise.each(fileUris, function(uri) {
+//     return spark.dlFiles(uri).then(function(payload) {
+//       console.log(payload.fileName);
+//     });
+//   }).then(function() {
+//     //Results have been settled
+//     console.log('files have been stored')
+//   })
+// })
+
+spark.getMessages({roomId: config.roomId}).then((data) => {
   var messages = data;
-
-  var fileUris = messages
-    .filter((message) => message.files)
-    .map((message) => message.files)
-    .reduce((arr, files) => arr.concat(files), []);
-
-  return Promise.each(fileUris, function(uri) {
-    return spark.dlFiles(uri).then(function(payload) {
-      console.log(payload.fileName);
-    });
-  }).then(function() {
-    //Results have been settled
-    console.log('files have been stored')
-  })
+  return spark.getFileUris(messages)
+}).then((fileUris) => {
+  console.log(fileUris);
 })
