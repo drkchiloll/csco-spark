7970da8980e31ea4afcb4bca35bc83bd65f7b51c
drkchiloll <samwomack_@me.com>
2016-01-15 13:51:58 -0600

    sharing an example webhook creation for the Library

---
+++ examples/webhooks.js
@@ -0,0 +1,20 @@
+var config = require('./config'),
+    SparkFactory = require('../csco-spark');
+
+var spark = SparkFactory({
+  uri: 'https://api.ciscospark.com/v1',
+  token: config.token
+});
+
+spark.addWebhook({
+  name: 'pubHooks',
+  hookUrl: 'http://45.55.244.195:8181/hooker',
+  roomId: config.roomId
+}).then(function(resp) {
+  // Do Not have to Perform JSON.parse()
+  console.log(resp.id);
+})
+
+// spark.deleteWebhook('Y2lzY29zcGFyazovL3VzL1dFQkhPT0svNTRkZmVjMzgtMmVlYy00ZDU4LWIyNjEtNzk2MTY2ZDlmY2Fm').then((resp) => {
+//   console.log(resp); // Expect Null
+// })
